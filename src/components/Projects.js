649809b9529fc3474b8ef052e6d3b4f3611164fb
wanja <114565394+WanjaNjunge@users.noreply.github.com>
2024-02-08 12:39:30 +0300

    Cypress project link update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -20,7 +20,7 @@ export const Projects = () => {
       title: "E2E Testing with Cypress",
       description: "Automation testing using Cypress on OpenCart's Storefront web app. Implementation of CI/CD workflow through integration with GitHub Actions",
       imgUrl: projImg2 ,
-      link: "https://github.com/WanjaNjunge/opencart/blob/master/e2e-testing/README.md"
+      link: "https://github.com/WanjaNjunge/e2e-testing-cypress/blob/main/README.md"
     },
     {
       title: "Manual Testing Documentation",
